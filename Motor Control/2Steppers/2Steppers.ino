fb53ee343a360045b89b06c28071c7184a128533
Heavy-Throws <HeavyThrowsDev@gmail.com>
2022-08-10 13:48:45 -0400

    Fixing timing error where data was not available but Tracker tries sending to Motor

--- Motor Control/2Steppers/2Steppers.ino
+++ Motor Control/2Steppers/2Steppers.ino
@@ -7,13 +7,17 @@
  */
 #include <Arduino.h>
 #include "BasicStepperDriver.h"
+#include "MultiDriver.h"
+#include "SyncDriver.h"
 
-#define MOTOR_STEPS 200
+//#define MOTOR_STEPS_PAN 600
+#define MOTOR_STEPS_TILT 200
 #define RPM 60         //180 seems to be max
-#define MOTOR_ACCEL 200 //Quite fast accel (600)
-#define MOTOR_DECEL 500 //Very fast decel (900)
+#define MOTOR_ACCEL 600 //Quite fast accel (600)
+#define MOTOR_DECEL 900 //Very fast decel (900)
 #define MICROSTEPS 64 // Hardware microstepping by driver
-#define REV_STEPS MICROSTEPS*MOTOR_STEPS
+#define REV_STEPS_PAN 38400
+#define REV_STEPS_TILT MICROSTEPS*MOTOR_STEPS_TILT
 
 #define PAN_DIR 4
 #define PAN_STEP 5
@@ -25,8 +29,9 @@
 
 #define SERIAL_BUFF 4
 unsigned char command[SERIAL_BUFF];
-BasicStepperDriver motorPAN(MOTOR_STEPS, PAN_DIR, PAN_STEP, PAN_ENABLE);
-BasicStepperDriver motorTILT(MOTOR_STEPS, TILT_DIR, TILT_STEP, TILT_ENABLE);
+BasicStepperDriver motorPAN(600, PAN_DIR, PAN_STEP, PAN_ENABLE);
+BasicStepperDriver motorTILT(MOTOR_STEPS_TILT, TILT_DIR, TILT_STEP, TILT_ENABLE);
+SyncDriver controller(motorPAN, motorTILT);
 
 float PanTargetAngle = 0;
 uint16_t PanTargetINT = 0;
@@ -49,9 +54,9 @@ void setup() {
     motorTILT.setSpeedProfile(motorTILT.LINEAR_SPEED, MOTOR_ACCEL, MOTOR_DECEL);
     motorTILT.enable();
     
-    Serial.println("Stepper Setup Complete");
-    Serial.print("Revolution steps:   ");
-    Serial.println(REV_STEPS);
+    //Serial.println("Stepper Setup Complete");
+    //Serial.print("Revolution steps:   ");
+    //Serial.println(REV_STEPS);
     Serial.println("READY");
 }
 
@@ -63,9 +68,9 @@ void loop() {
       Serial.readBytes(command,SERIAL_BUFF);
       PanTargetINT = (command[0]<<8) + command[1];
       PanTargetAngle = PanTargetINT*0.015;
-      Serial.write(PanTargetINT);
+      //Serial.write(PanTargetINT);
       PanTargetAngle = fmod(PanTargetAngle,360);
-      PanTargetSteps = (int)(PanTargetAngle*MOTOR_STEPS*MICROSTEPS/360);
+      PanTargetSteps = (int)(PanTargetAngle*600*MICROSTEPS/360);
 //      Serial.print("New target:     ");
 //      Serial.println(PanTargetAngle);
 //      Serial.print("Step target:    ");
@@ -74,7 +79,7 @@ void loop() {
       
       //Don't have to worry about timing because all this is done before nextAction()
       PanCurrentSteps += motorPAN.getDirection()*motorPAN.getStepsCompleted();
-      PanCurrentSteps %= REV_STEPS;
+      PanCurrentSteps %= 38400;
 //      Serial.print("Current Steps:  ");
 //      Serial.println(PanCurrentSteps);
 //      Serial.print("Direction:      ");
@@ -82,34 +87,34 @@ void loop() {
 
       
       //Find which direction is shortest
-      long stepDiff = PanTargetSteps-PanCurrentSteps; 
-      if (stepDiff > REV_STEPS/2 )
-        stepDiff -= REV_STEPS;
-      else if (stepDiff < -1*REV_STEPS/2)
-        stepDiff += REV_STEPS;
+      long stepDiffPan = PanTargetSteps-PanCurrentSteps; 
+      if (stepDiffPan > 38400/2 )
+        stepDiffPan -= 38400;
+      else if (stepDiffPan < -1*38400/2)
+        stepDiffPan += 38400;
         
 //      Serial.print("Diff:           ");
 //      Serial.println(stepDiff);
 //      Serial.println("~~~~~~~~~~~~~~~");
       //setup new move
-      motorPAN.startMove(stepDiff,0);
+      //motorPAN.startMove(stepDiff,0);
 
       //  TILT TILT TILT   //
       TiltTargetINT = (command[2]<<8) + command[3];
-      TiltTargetAngle = map(int(TiltTargetINT*0.015),0,360,0,255); //pwm so just need angle
-      Serial.write(TiltTargetINT);
+      TiltTargetAngle = TiltTargetINT*0.015; //pwm so just need angle
+      //Serial.write(TiltTargetINT);
       TiltTargetAngle = fmod(TiltTargetAngle,360);
-      TiltTargetSteps = (int)(TiltTargetAngle*MOTOR_STEPS*MICROSTEPS/360);
+      TiltTargetSteps = (int)(TiltTargetAngle*MOTOR_STEPS_TILT*MICROSTEPS/360);
       TiltCurrentSteps += motorTILT.getDirection()*motorTILT.getStepsCompleted();
-      TiltCurrentSteps %= REV_STEPS;
-      stepDiff = TiltTargetSteps-TiltCurrentSteps; 
-      if (stepDiff > REV_STEPS/2 )
-        stepDiff -= REV_STEPS;
-      else if (stepDiff < -1*REV_STEPS/2)
-        stepDiff += REV_STEPS;  
-      motorTILT.startMove(stepDiff,0);
+      TiltCurrentSteps %= REV_STEPS_TILT;
+      long stepDiff = TiltTargetSteps-TiltCurrentSteps; 
+      if (stepDiff > REV_STEPS_TILT/2 )
+        stepDiff -= REV_STEPS_TILT;
+      else if (stepDiff < -1*REV_STEPS_TILT/2)
+        stepDiff += REV_STEPS_TILT;  
+      //motorTILT.startMove(stepDiff,0);
+      controller.startMove(stepDiffPan, stepDiff);
     }
     
-    long wait_time_micros = motorPAN.nextAction();
-    
+    long wait_time_micros = controller.nextAction();
 }
--- Tracking Software/Tracker.py
+++ Tracking Software/Tracker.py
@@ -38,7 +38,8 @@ class AircraftHanger:
         with self._lock:
             if self.data:
                 for craft in self.data:
-                    print(f"{craft.callsign}", end = "")
+                    print(f"{craft.callsign}", end = "") 
+                print('')
 
 def APIFunction(hanger, user=None, pw=None):
     try:
@@ -64,52 +65,60 @@ def APIFunction(hanger, user=None, pw=None):
             return None
     
     
-def SerialFunction(hanger, geod):
+def SerialFunction(hanger):
     try:
         ardi = serial.Serial(port='COM3', baudrate=74880, timeout=.1)
     except serial.SerialException:
         print("COM Port cannot be opened")
         return None
+    print("Waiting for board")
     while True:
         resp = ardi.read_until('\r\n',5)
         if resp[:5].decode("utf-8") == 'READY':
             break;
     print("COM Port open!")
+    time.sleep(5)
+    geod = Geodesic.WGS84
     
     while(True):
         if hanger.data:
-            trackingCraft = ''
-            closestdist = 999999
-            for craft in hanger.data:
-                if (time.time()//1-craft.time_position < 20 and craft.on_ground == False):
-                    measure = geod.Inverse(me[0], me[1], craft.latitude, craft.longitude)
-                    #print(f"{craft.callsign} {measure.get('s12')}")
-                    if measure.get('s12') < closestdist:
-                        trackingCraft = craft.icao24
-                        closestdist = measure.get('s12')
-            elevation = 45
-            for craft in hanger.data:
-                if craft.icao24 == trackingCraft:
-                    newLatLon = geod.Direct(craft.latitude, craft.longitude, craft.true_track, craft.velocity*(time.time()-craft.time_position))
-                    #print(newLatLon, end="    <------")
-                    measure = geod.Inverse(me[0], me[1], newLatLon.get('lat2'), newLatLon.get('lon2'))
-                    #Prioritze geo altitude over baro. Default to 45deg.
-                    
-                    alt = None
-                    if craft.geo_altitude:
-                        elevation = math.degrees(math.atan(craft.geo_altitude/measure.get('s12')))           
-                        alt = craft.geo_altitude
-                    elif craft.baro_altitude:
-                        elevation = math.degrees(math.atan(craft.baro_altitude/measure.get('s12')))
-                        alt = craft.baro_altitude
-                    #print(f"\tDistance:\t{measure.get('s12')//1} \n\tAltitude:\t{alt//1}   \n\tAzimuth:\t{measure.get('azi1')//1}    \n\tElevation:\t{elevation//1}")
-                    break
+            with hanger._lock:
+                trackingCraft = ''
+                closestdist = 999999
+                for craft in hanger.data:
+                    if (time.time()//1-craft.time_position < 20 and craft.on_ground == False):
+                        measure = geod.Inverse(me[0], me[1], craft.latitude, craft.longitude)
+                        #print(f"{craft.callsign} {measure.get('s12')}")
+                        if measure.get('s12') < closestdist:
+                            trackingCraft = craft.icao24
+                            closestdist = measure.get('s12')
+                elevation = 45
+                #print(trackingCraft)
+                for craft in hanger.data:
+                    if craft.icao24 == trackingCraft:
+                        #print("Tracking")
+                        newLatLon = geod.Direct(craft.latitude, craft.longitude, craft.true_track, craft.velocity*(time.time()-craft.time_position))
+                        #print(newLatLon, end="    <------")
+                        measure = geod.Inverse(me[0], me[1], newLatLon.get('lat2'), newLatLon.get('lon2'))
+                        #Prioritze geo altitude over baro. Default to 45deg.
                         
-            ardi.reset_output_buffer()
-            ardi.reset_input_buffer() 
-            ardi.write(angle_to_hex(measure.get('azi1')).to_bytes(2,byteorder='big'))
-            ardi.write(angle_to_hex(elevation).to_bytes(2,byteorder='big'))
-            time.sleep(0.1)
+                        alt = 45
+                        if craft.geo_altitude:
+                            elevation = math.degrees(math.atan(craft.geo_altitude/measure.get('s12')))           
+                            alt = craft.geo_altitude
+                        elif craft.baro_altitude:
+                            elevation = math.degrees(math.atan(craft.baro_altitude/measure.get('s12')))
+                            alt = craft.baro_altitude
+                        print(f"{craft.callsign}\n\tDistance:\t{measure.get('s12')//1}\tAltitude:\t{alt//1}   \n\tAzimuth:\t{measure.get('azi1')//1} \tElevation:\t{elevation//1}")
+                        break          
+                if measure:
+                    ardi.reset_output_buffer()
+                    ardi.reset_input_buffer() 
+                    ardi.write(angle_to_hex(measure.get('azi1')).to_bytes(2,byteorder='big'))
+                    ardi.write(angle_to_hex(elevation).to_bytes(2,byteorder='big'))
+                    #print(angle_to_hex(measure.get('azi1')).to_bytes(2,byteorder='big'),end='')
+                    #print(angle_to_hex(elevation).to_bytes(2,byteorder='big'))
+            time.sleep(0.25)
     
 def angle_to_hex(ang):
     return int((ang%360)/0.015)
@@ -129,11 +138,11 @@ if __name__ == "__main__":
         apiThread = threading.Thread(target=APIFunction, daemon=True, kwargs={'hanger':hanger})
     apiThread.start()
     
-    serThread = threading.Thread(target=SerialFunction, daemon=True, args=(hanger, geod, ))
+    serThread = threading.Thread(target=SerialFunction, daemon=True, args=(hanger, ))
     serThread.start()
 
     while True:
-        hanger.print_data()    
+        #hanger.print_data()    
         time.sleep(5)
         
     print('Bye!')
